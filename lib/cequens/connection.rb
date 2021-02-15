d2fedd260d65003c6255eb7fc46236d64705d791
Ahmed A. Ibrahim <ahmed.saraya90@gmail.com>
2021-02-15 22:13:27 +0200

    added connection

---
+++ lib/cequens/connection.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'faraday'
+require 'json'
+
+module Cequens
+  class Connection
+    CEQUENS_BASE_URL = 'https://apis.cequens.com/'
+
+    class << self
+      def post(path, params, body)
+        connection.post(path) do |request|
+          request.params = params
+          request.body = body.to_json
+        end
+      end
+
+      private
+
+      def connection
+        @connection ||= Faraday.new(url: CEQUENS_BASE_URL,
+                                    headers: { 'Content-Type': 'application/json',
+                                               'Accept': 'application/json' })
+      end
+    end
+  end
+end
